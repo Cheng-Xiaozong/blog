4cc8286aca107a1f40f1c0192d76cd4629da45b3
guimin <1766693494@qq.com>
2017-07-27 09:56:08 +0800

    主页评论

--- resources/views/ariticle/index.blade.php
+++ resources/views/ariticle/index.blade.php
@@ -1,4 +1,9 @@
 @extends('layouts.app')
+@section('style')
+    <link rel="stylesheet" href="{{asset('home/css/comment.css')}}">
+    <link rel="stylesheet" href="{{asset('home/css/style.css')}}">
+
+@show
 @section('content')
     <div class="home-content g-container">
         <div class="home-content-main g-container-main">
@@ -51,7 +56,40 @@
                         <img src="{{!empty($newAriticle->head_portrait) ? $newAriticle->head_portrait : asset('home/img/pageHome/usericon02.png')}}"/>
                         <span class="authorname g-pointer g-hover"> {{$newAriticle->author}}</span>
                     </div>
-                    <textarea class="blogcontent-comment">发表评论。。。</textarea>
+                    <!-- sdfsdfsddsssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss -->
+                    <div class="commentAll">
+                        <!--评论区域 begin-->
+                        <div class="reviewArea clearfix">
+                            <textarea class="content comment-input" placeholder="Please enter a comment&hellip;" onkeyup="keyUP(this)"></textarea>
+                            <a href="javascript:;" class="plBtn">评论</a>
+                        </div>
+                        <!--评论区域 end-->
+                        <!--回复区域 begin-->
+                        <div class="comment-show">
+                            <div class="comment-show-con clearfix">
+                                <div class="comment-show-con-img pull-left"><img src="" alt=""></div>
+                                <div class="comment-show-con-list pull-left clearfix">
+                                    <div class="pl-text clearfix">
+                                        <a href="#" class="comment-size-name">张三 : </a>
+                                        <span class="my-pl-con">&nbsp;来啊 造作啊!</span>
+                                    </div>
+                                    <div class="date-dz">
+                                        <span class="date-dz-left pull-left comment-time">2017-5-2 11:11:39</span>
+                                        <div class="date-dz-right pull-right comment-pl-block">
+                                            <a href="javascript:;" class="removeBlock">删除</a>
+                                            <a href="javascript:;" class="date-dz-pl pl-hf hf-con-block pull-left">回复</a>
+                                            <span class="pull-left date-dz-line">|</span>
+                                            <a href="javascript:;" class="date-dz-z pull-left"><i class="date-dz-z-click-red"></i>赞 (<i class="z-num">666</i>)</a>
+                                        </div>
+                                    </div>
+                                    <div class="hf-list-con"></div>
+                                </div>
+                            </div>
+                        </div>
+                        <!--回复区域 end-->
+                    </div>
+                    <!-- sdfsddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd -->
+                   <!--  <textarea class="blogcontent-comment">发表评论。。。</textarea>
                     <ul class="blogcontent-commentlist">
                         <li>
                             <img src="{{asset('home/img/pageHome/home-author-icon.png')}}"/>
@@ -67,7 +105,7 @@
                                 <p class="commentlist-infobottom">这篇文章针砭时弊，批评世俗，反思历史，解读人生，公正深刻，辛辣幽默，雅俗共赏，生动鲜活，甚是好文章哟 ！</p>
                             </div>
                         </li>
-                    </ul>
+                    </ul> -->
                     <div class="blogcontent-loadmore g-btn">加载更多精彩评论...</div>
                 </div>
                 @endif
@@ -77,6 +115,11 @@
     
 @stop
 @section('javascript')
+    <script src="{{asset('home/js/jquery.min.js')}}"></script>
     <script src="{{asset('home/js/home.js')}}"></script>
+    <script src="{{asset('home/js/global.js')}}"></script>
+    <script type="text/javascript" src="{{asset('home/js/jquery.flexText.js')}}"></script>
+    <script type="text/javascript " src="{{asset('home/js/myBlogDiscuss.js')}}"></script>
+    <script src="{{asset('home/js/myBlog.js')}}"></script>
 @stop
 
